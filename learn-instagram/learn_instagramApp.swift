b000490437807b853d1fc9c71d8cffae66c79dfe
wilbertdevin <wilbertdevin888@gmail.com>
2023-11-18 14:03:23 +0700

    Initial Commit

---
+++ learn-instagram/learn_instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_instagramApp.swift
+//  learn-instagram
+//
+//  Created by Wilbert Devin Wijaya on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct learn_instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
