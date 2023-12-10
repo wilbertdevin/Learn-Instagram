cba1d19f91ed211b3e3cfa51c599aac06c8a6701
wilbertdevin <wilbertdevin888@gmail.com>
2023-12-10 16:05:28 +0700

    profile action button

--- learn-instagram/Core/Profile/ProfileHeaderView.swift
+++ learn-instagram/Core/Profile/ProfileHeaderView.swift
@@ -56,16 +56,24 @@ struct ProfileHeaderView: View {
             // action button
             
             Button(action: {
+                if user.isCurrentUser {
+                    print("Show edit profile")
+                } else {
+                    print("Follow user..")
+                }
                 
             }, label: {
-                Text("Edit Profile")
+                Text(user.isCurrentUser ? "Edit Profile" : "Follow")
                     .font(.subheadline)
                     .fontWeight(.semibold)
                     .frame(width: 360, height: 32)
+                    .background(user.isCurrentUser ? .white : Color(.systemBlue))
+                    .foregroundColor(user.isCurrentUser ? .black : .white)
+                    .cornerRadius(6)
                     .foregroundColor(.black)
                     .overlay {
                         RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.gray, lineWidth: 1)
+                            .stroke(user.isCurrentUser ? .gray : .clear, lineWidth: 1)
                     }
             })
             
--- learn-instagram/Model/User.swift
+++ learn-instagram/Model/User.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Firebase
 
 struct User: Identifiable, Hashable, Codable {
     let id: String
@@ -15,6 +16,10 @@ struct User: Identifiable, Hashable, Codable {
     var bio: String?
     let email: String
     
+    var isCurrentUser: Bool {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return false }
+        return currentUid == id
+    }
 }
 
 extension User {
