d870e9f07231a85ad1dbfd92a5d91419a7ff45c4
wilbertdevin <wilbertdevin888@gmail.com>
2023-12-15 17:23:13 +0700

    fetch display images from firebase storage

--- learn-instagram/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ learn-instagram/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -26,6 +26,15 @@ class EditProfileViewModel: ObservableObject {
     
     init(user: User) {
         self.user = user
+        
+        if let fullname = user.fullname {
+            self.fullname = fullname
+        }
+        
+        if let bio = user.bio {
+            self.bio = bio
+        }
+        
     }
     
     func loadImage(fromItem item: PhotosPickerItem?) async {
