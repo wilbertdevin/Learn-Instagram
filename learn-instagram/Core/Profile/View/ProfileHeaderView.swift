1f9f2de08aa090b2b2a118feff2cf2f57f32f25a
wilbertdevin <wilbertdevin888@gmail.com>
2023-12-15 09:01:02 +0700

    update user data

--- learn-instagram/Core/Profile/View/ProfileHeaderView.swift
+++ learn-instagram/Core/Profile/View/ProfileHeaderView.swift
@@ -48,8 +48,6 @@ struct ProfileHeaderView: View {
                         .font(.footnote)
                 }
                 
-                Text(user.username)
-
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.horizontal)
@@ -81,7 +79,7 @@ struct ProfileHeaderView: View {
             Divider()
         }
         .fullScreenCover(isPresented: $showEditProfile, content: {
-            Text("Edit profile view")
+            EditProfileView(user: user)
         })
     }
 }
