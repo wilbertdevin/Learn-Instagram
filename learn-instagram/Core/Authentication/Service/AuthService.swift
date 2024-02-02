9756a1d7259fac1245da2017b43d24482d8ad57f
wilbertdevin <wilbertdevin888@gmail.com>
2024-02-02 20:22:57 +0700

    Fetch Feed Posts

--- learn-instagram/Core/Authentication/Service/AuthService.swift
+++ learn-instagram/Core/Authentication/Service/AuthService.swift
@@ -51,8 +51,7 @@ class AuthService {
         self.userSession = Auth.auth().currentUser
         
         guard let currentUid = userSession?.uid else { return }
-        let snapshot = try await Firestore.firestore().collection("users").document(currentUid).getDocument()
-        self.currentUser = try? snapshot.data(as: User.self)
+        self.currentUser = try await UserService.fetchUser(withUid: currentUid)
     }
     
     func signOut() {
